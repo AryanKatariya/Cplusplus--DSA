33e5ad1edb7639072841775b73a76f4e3f048b3b
AryanKatariya <aryankatariya21@gmail.com>
2023-10-11 23:26:58 +0530

    11/10/23

---
+++ array/twoSum.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+        vector<int> nums2 = nums;
+        
+        sort(nums2.begin(), nums2.end());
+
+        int a, b;
+        int l = 0;
+        int r = n-1;
+        while(l<=r){
+            if(nums2[l] + nums2[r] < target){
+                ++l;
+            }else if(nums2[l] + nums2[r] > target){
+                --r;
+            }else{
+                a = nums2[l];
+                b = nums2[r];
+                break;
+            }
+        }   
+
+         vector<int> ans(2);
+        
+        for(int i = 0; i < n; ++i){
+            if(nums[i] == a){
+                ans[0] = i;
+                break;
+            }
+        }
+        
+        for(int i = n-1; i >= 0; --i){
+            if(nums[i] == b){
+                ans[1] = i;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+
